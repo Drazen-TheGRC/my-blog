143124ac0280f1888f575c9436c3dbb1f2154616
Drazen Drinic <drazendrinic@hotmail.com>
2023-11-09 16:31:25 -0600

    Add: pages outline & react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesPage from './pages/ArticlesPage';
 
 function App() {
   return (
     <div className='App'>
       <h1>My Awesome Blog</h1>
-      <div className='page-body'>Welcome to my blog</div>
+      <div className='page-body'>
+        <HomePage />
+        <AboutPage />
+        <ArticlesPage />
+        <ArticlePage />
+      </div>
     </div>
   );
 }
