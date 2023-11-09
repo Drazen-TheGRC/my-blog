b8b5038fdc4bca0314ec3585e3f1dad8dd07cd77
Drazen Drinic <drazendrinic@hotmail.com>
2023-11-09 16:39:27 -0600

    Setup: routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -7,15 +7,19 @@ import ArticlesPage from './pages/ArticlesPage';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>My Awesome Blog</h1>
-      <div className='page-body'>
-        <HomePage />
-        <AboutPage />
-        <ArticlesPage />
-        <ArticlePage />
+    <BrowserRouter>
+      <div className='App'>
+        <h1>My Awesome Blog</h1>
+        <div className='page-body'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles' element={<ArticlesPage />} />
+            <Route path='/articles:articleId' element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
