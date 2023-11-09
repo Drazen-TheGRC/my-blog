c9311ebcba6d2c94a0e29e6accffcf8eb444ffc1
Drazen Drinic <drazendrinic@hotmail.com>
2023-11-09 17:07:20 -0600

    Add: NavBar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticlesPage from './pages/ArticlesPage';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-        <h1>My Awesome Blog</h1>
-        <div className='page-body'>
+        <NavBar />
+        <div id='page-body'>
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/about' element={<AboutPage />} />
