4df8c0f0a6934b875657b0563777bb5bed9a1b1a
Drazen Drinic <drazendrinic@hotmail.com>
2023-11-10 02:37:06 -0600

    Add: list articles to the  ArticlesPage

--- src/pages/ArticlesPage.jsx
+++ src/pages/ArticlesPage.jsx
@@ -1,5 +1,22 @@
+import articles from './article-content';
+import { Link } from 'react-router-dom';
+
 const ArticlesPage = () => {
-  return <h1>This is the ArticlesPage</h1>;
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles.map((article) => (
+        <Link
+          to={`/articles/${article.name}`}
+          className='article-list-item'
+          key={article.name}
+        >
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}...</p>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default ArticlesPage;
