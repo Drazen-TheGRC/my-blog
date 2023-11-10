b50b6812e38ee0fd6aa020a4e9785e4d571b5b08
Drazen Drinic <drazendrinic@hotmail.com>
2023-11-10 11:45:14 -0500

    Test: URL params with Postman

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,10 +4,22 @@ const app = express();
 
 app.use(express.json());
 
+//
+
 app.post('/hello', (req, res) => {
   console.log(req.body);
   res.send(`Hello ${req.body.name}`);
 });
+
+//
+
+app.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
+  console.log(req.params);
+  res.send(`Hello ${name}!!!`);
+});
+
+//
 
 app.listen(8000, () => {
   console.log('Server is listening on port 8000');
