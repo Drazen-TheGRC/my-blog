e13350a6e0c30db8b7cb893279ef5646420afd21
Drazen Drinic <drazendrinic@hotmail.com>
2023-11-10 12:16:58 -0500

    Test: upvote article using PUT and Postman

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,7 +1,22 @@
 import express from 'express';
 
-const app = express();
+// temp dummy data
+let articleInfo = [
+  {
+    name: 'learn-react',
+    upvotes: 0,
+  },
+  {
+    name: 'learn-node',
+    upvotes: 0,
+  },
+  {
+    name: 'mongodb',
+    upvotes: 0,
+  },
+];
 
+const app = express();
 app.use(express.json());
 
 //
@@ -11,13 +26,35 @@ app.post('/hello', (req, res) => {
   res.send(`Hello ${req.body.name}`);
 });
 
+// Upvote the article
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+  // upvote logic
+  const { name } = req.params;
+  const article = articleInfo.find((a) => a.name === name);
+  if (article) {
+    article.upvotes++;
+    res.send(`The ${name} article has ${article.upvotes} upvotes`);
+  } else {
+    res.send("That article doesn't exist");
+  }
+});
+
 //
 
-app.get('/hello/:name', (req, res) => {
-  const name = req.params.name;
-  console.log(req.params);
-  res.send(`Hello ${name}!!!`);
-});
+// app.get('/hello/:name', (req, res) => {
+//   const name = req.params.name;
+//   console.log(req.params);
+//   res.send(`Hello ${name}!!!`);
+// });
+
+//
+
+// app.get('/hello/:name/goodbye/:otherName', (req, res) => {
+//   const name = req.params.name;
+//   console.log(req.params);
+//   res.send(`Hello ${name}!!!`);
+// });
 
 //
 
