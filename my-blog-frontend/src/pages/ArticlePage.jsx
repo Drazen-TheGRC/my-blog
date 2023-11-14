1ef53d5156a9453ba479295c701c1106ebcaf623
Drazen Drinic <drazendrinic@hotmail.com>
2023-11-14 13:48:42 -0500

    Update: ArticlePage to use Axios

--- my-blog-frontend/src/pages/ArticlePage.jsx
+++ my-blog-frontend/src/pages/ArticlePage.jsx
@@ -1,14 +1,23 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import articles from './article-content';
 import NotFoundPage from './NotFounPage';
 
 const ArticlePage = () => {
-  // const params = useParams();
-  // const articleId = params.articleId;
-  // or
-  // const {articleId} = params
-
   const { articleId } = useParams();
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      // Get res
+      const response = await axios.get(`/api/articles/${articleId}`);
+      //
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+    loadArticleInfo();
+  }, [articleId]);
 
   const article = articles.find((article) => article.name === articleId);
 
@@ -18,6 +27,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This artice has {articleInfo.upvotes} upvote(s)</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
