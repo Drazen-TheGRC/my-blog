9a29fcde9cfeadcc066b7afd48a8bfdc70aabae2
Drazen Drinic <drazendrinic@hotmail.com>
2023-11-15 14:06:42 -0500

    Create: log in form

--- my-blog-frontend/src/pages/LogInPage.jsx
+++ my-blog-frontend/src/pages/LogInPage.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const LogInPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  const logIn = async () => {
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      navigate('/articles');
+    } catch (e) {
+      setError(e.message);
+    }
+  };
 
   return (
     <>
@@ -27,7 +39,7 @@ const LogInPage = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
 
-      <button>Log In</button>
+      <button onClick={logIn}>Log In</button>
       <Link to='/create-account'>Don't have an account? Create one here</Link>
     </>
   );
